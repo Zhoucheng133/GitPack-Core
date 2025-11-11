4c5b88b4a3f9d74c075b85a886bc0a2e62f1054c
zhoucheng <zhoucheng133@outlook.com>
2025-11-11 21:00:03 +0800

    添加动态库函数

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"fmt"
 	"gitpack-core/utils"
+
+	"C"
 )
+
+//export RepoToNew
+func RepoToNew(repoPath *C.char, outputPath *C.char) C.int {
+	err := utils.RepoToNew(C.GoString(repoPath), C.GoString(outputPath))
+
+	if err != nil {
+		return 1
+	}
+	return 0
+}
 
 func main() {
 	err := utils.RepoToNew("/Users/zhoucheng/Desktop/Develop/DAV-Server", "/Users/zhoucheng/Downloads")
